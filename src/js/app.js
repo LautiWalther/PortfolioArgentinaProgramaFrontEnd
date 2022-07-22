df7339fa586df185fa5926fbfc330c9b0d4b86f8
LautiWalther <lautigeniop@gmail.com>
2022-07-22 15:29:24 -0300

    added bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-class Persona {
+class Person {
     constructor(name, lastname, dateofBirdth, languages) {
         this.name = name;
         this.lastname = lastname;
@@ -28,10 +28,38 @@ class Persona {
         this.languages = newLangs;
     }
 }
-class User extends Persona {
-    constructor(name, lastname, dateofBirdth, languages, admin) {
+class User extends Person {
+    constructor(name, lastname, dateofBirdth, languages, admin, level, experience) {
         super(name, lastname, dateofBirdth, languages);
+        this.level = level;
+        this.experience = experience;
         this.admin = admin;
+    }
+    getAdmin() {
+        return this.admin;
+    }
+    setAdmin(newLevel) {
+        this.admin = newLevel;
+    }
+    getExperience() {
+        return this.experience;
+    }
+    setExperience(newExp) {
+        this.experience = newExp;
+    }
+    getLevel() {
+        return this.level;
+    }
+    setLevel(newLevel) {
+        this.level = newLevel;
+    }
+    levelUp() {
+        this.setLevel(this.getLevel() + 1);
+        this.setExperience(0);
+    }
+    levelDown() {
+        this.setLevel(this.getLevel() - 1);
+        this.setExperience(0);
     }
 }
 var user = new User('Lautaro', 'Walther', '25-02-2003', [
@@ -53,5 +81,5 @@ var user = new User('Lautaro', 'Walther', '25-02-2003', [
         },
         avgLevel: 8.67
     }
-], 1);
+], 1, 6, 1437);
 console.log(user);
